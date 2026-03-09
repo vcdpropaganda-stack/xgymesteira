cf5e1da00f3df684996d60285227f702d6ac191b
Bruno Ravaglia <bumax22@gmail.com>
2026-03-09 03:38:03 -0300

    Hardcode GTM in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { GTMManager } from "@/components/gtm-manager";
 import "./globals.css";
+
+const GTM_ID = "GTM-N645NPMW";
 
 export const metadata: Metadata = {
   title: "XGYM Body Shape | Agende sua sessão pelo WhatsApp",
@@ -36,7 +39,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script id="xgym-gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <GTMManager />
       </body>
